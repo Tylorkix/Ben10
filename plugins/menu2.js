193b8c22f7eaa75b5574167a9d3e88a9fe794f94
Tylor White <tylorknine9@gmail.com>
2025-06-04 18:14:51 +0300

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -294,7 +294,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363290715861418@newsletter',
+                        newsletterJid: '0029VaYpDLx4tRrrrXsOvZ3U@newsletter',
                         newsletterName: config.BOT_NAME,
                         serverMessageId: 143
                     }
